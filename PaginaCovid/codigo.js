2e65ae1d25dc7ad51dfbf7e62354922c41048319
Alexz123640 <u19101608@utp.edu.pe>
2021-05-25 09:43:25 -0500

    covid

---
+++ PaginaCovid/codigo.js
@@ -0,0 +1,14 @@
+
+// sidebar toggle
+
+const btnToggle = document.querySelector(".toggle-btn");
+console.log(btnToggle);
+
+btnToggle.addEventListener("click", function () {
+  console.log('clik')
+  document.getElementById('sidebar').classList.toggle('active');
+  console.log(document.getElementById('sidebar'))
+});
+
+swal ( " ¡Estamos contigo en esto! " ) ;
+
